1aaea76ebe7f01a3954dbe4eaf1672f149953853
Paulo Fernandes <paulor.exe@gmail.com>
2024-04-20 14:35:51 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import fastify from "fastify";
+
+const app = fastify()
+
+app.get('/', (request, reply) => {
+  console.log(`[${request.method}] [${request.url}]`)
+  return 'Hello World!'
+})
+
+app.listen({
+   port: 3333
+}).then(() => {
+  console.log("HTTP server running")
+})
+
