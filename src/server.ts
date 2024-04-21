5c83d06b5862abb868112d933be94b67260b8563
Paulo Fernandes <paulor.exe@gmail.com>
2024-04-21 18:36:22 -0300

    config: eslint configured

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const app = fastify()
 
-app.get('/', (request, reply) => {
+app.get('/', (request) => {
   console.log(`[${request.method}] [${request.url}]`)
   return 'Hello World!'
 })
 
-app.listen({
-   port: 3333
-}).then(() => {
-  console.log("HTTP server running")
-})
+app
+  .listen({
+    port: 3333,
+  })
 
+  .then(() => {
+    console.log('HTTP server running')
+  })
