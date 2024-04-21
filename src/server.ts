990f427386c95c4859afe71463c45ff3663da2c3
Paulo Fernandes <paulor.exe@gmail.com>
2024-04-21 19:02:58 -0300

    config: db connection configured

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
-app.get('/', (request) => {
-  console.log(`[${request.method}] [${request.url}]`)
-  return 'Hello World!'
+app.get('/', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+  return tables
 })
 
 app
   .listen({
     port: 3333,
   })
-
   .then(() => {
     console.log('HTTP server running')
   })
